2c8ba064e684c6516893daf4059c2fa8711da0ad
Drishtant Regmi <drishtantregmi777@gmail.com>
2021-07-28 09:35:28 +0545

    update watchlist remove

--- src/components/WatchList.js
+++ src/components/WatchList.js
@@ -1,72 +1,80 @@
 import styled from "styled-components";
 import { selectList } from "../features/list/listSlice";
-import {useEffect} from 'react';
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
 import db from "../firebase";
-import { setList } from "../features/list/listSlice"
-import { selectUserName } from '../features/user/userSlice';
-
+import { setList } from "../features/list/listSlice";
+import { selectUserName } from "../features/user/userSlice";
 
 const WatchList = () => {
-    const dispatch = useDispatch();
-    const userName = useSelector(selectUserName);
-    let watchLists = [];
-    
-    const lists = useSelector(selectList);
+  const dispatch = useDispatch();
+  const userName = useSelector(selectUserName);
+  let watchLists = [];
+  const [show, setShow] = useState(true);
 
-    useEffect(()=>{
-        db.collection('lists').onSnapshot((snapshot) => {
-            snapshot.docs.map((doc) => {
-              watchLists = [...watchLists, { id: doc.id, ...doc.data()}]
-            });
-            dispatch(setList({
-                watchList: watchLists,
-                })
-            );
-        });
-    },[userName]);
+  const lists = useSelector(selectList);
 
-    const removeFromWatchList = (id) => {
-      db.collection('lists')
+  useEffect(() => {
+    db.collection("lists").onSnapshot((snapshot) => {
+      snapshot.docs.map((doc) => {
+        watchLists = [...watchLists, { id: doc.id, ...doc.data() }];
+      });
+      dispatch(
+        setList({
+          watchList: watchLists,
+        })
+      );
+    });
+  }, [userName]);
+
+  const removeFromWatchList = async (id) => {
+    await db
+      .collection("lists")
       .doc(id)
       .delete()
       .then(() => {
         console.log("successfully removed");
       })
       .catch((error) => {
-        console.error("",error)
-      })
-    }
+        console.error("", error);
+      });
+    setShow(!show);
+  };
 
-    return (
-        <Container>
-        <h4>WATCHLIST</h4>
-        <Content>
-            {lists &&
-            lists.map((list, key) => (
-                <Wrap key={key}>
-                {list.id}
-                <Link to={`/detail/` + list.id}>
-                  <img src={list.cardImg} alt={list.title} />
-                  <Remove onClick={() => {removeFromWatchList(list.id)}}>
-                    <img src="/images/remove.png" alt = "remove-btn" />  
-                  </Remove>
-                </Link>
-                </Wrap>
-            ))}
-        </Content>
-        </Container>
-        );
-}
+  return (
+    <Container>
+      <h4>WATCHLIST</h4>
+      <Content>
+        {lists &&
+          show &&
+          lists.map((list, key) => (
+            <Wrap key={key}>
+              {list.id}
+              <Link to={`/detail/` + list.id}>
+                <img src={list.cardImg} alt={list.title} />
+              </Link>
+              <Remove
+                onClick={() => {
+                  removeFromWatchList(list.id);
+                }}
+              >
+                <img src="/images/remove.png" alt="remove-btn" />
+              </Remove>
+            </Wrap>
+          ))}
+      </Content>
+    </Container>
+  );
+};
 
 const Container = styled.div`
-    position: relative;
-    min-height: calc(100vh - 250px);
-    overflow-x: hidden;
-    display: block;
-    top: 72PX;
-    padding: 0 calc(3.5vw + 5px);
+  position: relative;
+  min-height: calc(100vh - 250px);
+  overflow-x: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.5vw + 5px);
 `;
 
 const Content = styled.div`
@@ -74,7 +82,7 @@ const Content = styled.div`
   grid-gap: 40px;
   gap: 40px;
   grid-template-columns: repeat(4, minmax(0, 1fr));
-  
+
   @media (max-width: 768px) {
     grid-template-columns: repeat(2, minmax(0, 1fr));
   }
@@ -103,31 +111,27 @@ const Wrap = styled.div`
     width: 100%;
     z-index: 1;
     top: 0;
-    
   }
   &:hover {
     box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
       rgb(0 0 0 / 72%) 0px 30px 22px -10px;
     transform: scale(1.05);
     border-color: rgba(249, 249, 249, 0.8);
-
   }
 `;
 
 const Remove = styled.button`
+  img {
+    margin-left: 300px;
+    margin-top: 10px;
+    width: 25px;
+    height: 25px;
+    z-index: 1;
 
-      img {
-        margin-left: 300px;
-        margin-top: 10px;
-        width: 25px;
-        height: 25px;
-        z-index: 1;
-
-        &:hover {
-          cursor: pointer;
-        }
-      }
+    &:hover {
+      cursor: pointer;
+    }
+  }
 `;
 
 export default WatchList;
-
